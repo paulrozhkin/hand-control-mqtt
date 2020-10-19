b1d64ec9ce30d57e89e3a17329209753677e3ec4
Nikita Biryulin <nikita.biryulin@inventale.com>
2020-10-20 01:24:32 +0400

    Use maven instead of gradle.

---
+++ server/README.md
@@ -0,0 +1,7 @@
+# Building
+
+Use *mvn clean install* 
+
+In case if you have troubles with generated proto files:
+
+Right click on module -> Maven -> Reimport 
--- server/src/main/kotlin/com/handcontrol/server/Application.kt
+++ server/src/main/kotlin/com/handcontrol/server/Application.kt
@@ -1,12 +1,20 @@
 package com.handcontrol.server
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
-@SpringBootApplication
-class Application {
-    fun main(args: Array<String>) {
-        runApplication<Application>(*args)
+@SpringBootApplication(
+        exclude = [DataSourceAutoConfiguration::class]
+)
+open class Application{
+    @Bean
+    open fun service(): HandleRequestImpl {
+        return HandleRequestImpl()
     }
+}
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
