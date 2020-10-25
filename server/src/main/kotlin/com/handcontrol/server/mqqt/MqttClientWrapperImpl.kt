450dcc291998139c5e079e36531f153227e8b4e0
ritt <margarita.fedotova@rtlabs.ru>
2020-10-25 15:02:11 +0300

    review: add clientId to log, renaming, add vertx version variable

--- server/src/main/kotlin/com/handcontrol/server/mqqt/MqttClientWrapperImpl.kt
+++ server/src/main/kotlin/com/handcontrol/server/mqqt/MqttClientWrapperImpl.kt
@@ -13,69 +13,81 @@ import javax.annotation.PostConstruct
 class MqttClientWrapperImpl(private val mqttProps: MqttProperties) : MqttClientWrapper {
     private val logger = LoggerFactory.getLogger(MqttClientWrapperImpl::class.java)
 
-    private val client: MqttClient = MqttClient.create(Vertx.vertx())
+    private val client = MqttClient.create(Vertx.vertx())
+    private lateinit var clientId: String
 
     @PostConstruct
     fun init() {
         client.publishHandler { s: MqttPublishMessage ->
             val message = String(s.payload().bytes)
-            logger.info("Receive message with content: \"{}\" from topic {}", message, s.topicName())
-            // handle it
+            logger.info("Client {} received message with content: \"{}\" from topic {}",
+                    clientId, message, s.topicName())
+            // todo handle it
         }
-
         connect()
     }
 
     override fun connect() {
         if (client.isConnected) {
-            logger.warn("Client is already connected to broker")
+            logger.warn("Client {} is already connected to broker", clientId)
             return
         }
 
         client.connect(mqttProps.brokerPort, mqttProps.brokerAddr) { ch ->
             if (ch.succeeded()) {
-                logger.info("Connected to a mqtt broker")
+                clientId = client.clientId()
+                logger.info("Client {} connected to a mqtt broker", clientId)
+                // todo subscribe to necessary topics
                 subscribe("hello/world")
                 publish("hello/world", "I'm here")
-                // subscribe to necessary topics
             } else {
-                logger.error("Failed to connect to a mqtt broker. Reason: {}", ch.cause().message)
+                logger.error("Client {} failed to connect to a mqtt broker. Reason: {}",
+                        clientId, ch.cause().message)
             }
         }
     }
 
     override fun disconnect() {
-        logger.info("Disconnected from a mqtt broker")
-        client.disconnect();
+        client.disconnect { dh ->
+            if (dh.succeeded()) {
+                logger.info("Client {} disconnected from a mqtt broker", clientId)
+            } else {
+                logger.error("Client {} failed to disconnect from a mqtt broker. Reason: {}",
+                        clientId, dh.cause().message)
+            }
+        }
     }
 
     override fun subscribe(topic: String) {
-        client.subscribe(topic, mqttProps.qosLevel) { ch ->
-            if (ch.succeeded()) {
-                logger.info("Subscribe to topic {}", topic)
+        client.subscribe(topic, mqttProps.qosLevel) { sh ->
+            if (sh.succeeded()) {
+                logger.info("Client {} subscribed to topic {}", clientId, topic)
             } else {
-                logger.error("Failed to subscribe to topic: {}. Reason: {}", topic, ch.cause().message)
+                logger.error("Client {} failed to subscribe to topic: {}. Reason: {}",
+                        clientId, topic, sh.cause().message)
             }
         }
     }
 
     override fun unsubscribe(topic: String) {
-        client.unsubscribe(topic) { ch ->
-            if (ch.succeeded()) {
-                logger.info("Unsubscribe to topic {}", topic)
+        client.unsubscribe(topic) { sh ->
+            if (sh.succeeded()) {
+                logger.info("Client {} unsubscribed to topic {}", clientId, topic)
             } else {
-                logger.error("Failed to unsubscribe to topic: {}. Reason: {}", topic, ch.cause().message)
+                logger.error("Client {} failed to unsubscribe to topic: {}. Reason: {}",
+                        clientId, topic, sh.cause().message)
             }
         }
     }
 
     override fun publish(topic: String, msg: String) {
         client.publish(topic, Buffer.buffer(msg),
-                MqttQoS.valueOf(mqttProps.qosLevel), false, false) { ch ->
-            if (ch.succeeded()) {
-                logger.info("Publish msg \"{}\" to topic {}", msg, topic)
+                MqttQoS.valueOf(mqttProps.qosLevel), false, false) { sh ->
+            if (sh.succeeded()) {
+                logger.info("Client {} published msg \"{}\" to topic {}", clientId, msg, topic)
             } else {
-                logger.error("Failed to publish msg to topic: {}. Reason: {}", topic, ch.cause().message)
+                logger.error("Client {} failed to publish msg to topic: {}. Reason: {}",
+                        clientId, topic, sh.cause().message)
             }
         }
     }
