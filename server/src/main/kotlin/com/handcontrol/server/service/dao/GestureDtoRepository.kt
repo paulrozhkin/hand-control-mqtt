fc81824f513a4be73a9834579a38939b14007ff9
Nikita Biryulin <nikita.biryulin@inventale.com>
2020-12-11 02:06:14 +0300

    Delete unused classes.

--- server/src/main/kotlin/com/handcontrol/server/service/dao/GestureDtoRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.handcontrol.server.service.dao
-
-import com.handcontrol.server.mqtt.command.dto.gesture.GestureDto
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-
-//@Repository
-interface GestureDtoRepository: CrudRepository<GestureDto, String>
