3024773780f963fc7570106c1dbc8cec2cf7baad
ritt <margarita.fedotova@rtlabs.ru>
2020-12-13 12:19:55 +0300

    implement streaming telemetry

--- server/src/main/kotlin/com/handcontrol/server/mqtt/command/get/GetTelemetry.kt
+++ server/src/main/kotlin/com/handcontrol/server/mqtt/command/get/GetTelemetry.kt
@@ -4,6 +4,11 @@ import com.handcontrol.server.mqtt.command.DynamicCommand
 import com.handcontrol.server.mqtt.command.dto.TelemetryDto
 import com.handcontrol.server.mqtt.command.enums.DynamicApi.DynamicTopic.GET_TELEMETRY
 import com.handcontrol.server.util.ProtobufSerializer
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.ExperimentalSerializationApi
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -13,14 +18,34 @@ import org.springframework.stereotype.Component
  *  Updates every second
  */
 @Component
+@ExperimentalCoroutinesApi
 @ExperimentalSerializationApi
 class GetTelemetry : DynamicCommand(GET_TELEMETRY) {
     private val logger = LoggerFactory.getLogger(GetTelemetry::class.java)
 
+    private val subscribers = mutableMapOf<String, Channel<TelemetryDto>>()
+
     override fun handlePayloadAndId(id: String, byteArray: ByteArray) {
         val telemetry = ProtobufSerializer.deserialize<TelemetryDto>(byteArray)
         logger.debug("Get telemetry from {}: {}", id, telemetry)
-        // todo save it?
+
+        val sub = subscribers[id]
+        if (sub == null) {
+            logger.debug("No subscribers for telemetry from id: {}", id)
+            return
+        }
+
+        runBlocking {
+            sub.send(telemetry)
+        }
     }
 
+    fun subscribe(id: String) {
+        subscribers[id] = Channel(CONFLATED)
+    }
+
+    @FlowPreview
+    fun getChannel(id: String): Channel<TelemetryDto> {
+        return subscribers[id]!!
+    }
 }
